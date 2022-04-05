fa7537613445e9aa76f95031c590014e486d0f09
Tanya <tanyaborisenko94@gmail.com>
2022-04-05 20:47:58 +0300

    Initial commit

---
+++ Internship.Connect.QA.API.AutomationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Internship.Connect.QA.API.AutomationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
