da7fb18b1a856a2c0217629a047c2d9689c39465
Aliaksandr.Znak <aznak@itventure.com>
2022-04-18 21:04:16 +0300

    implemented extended restclient

---
+++ Internship.Connect.QA.API.AutomationTests/RestClientExtension/ExtendedRestClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Internship.Connect.QA.API.AutomationTests.Utils;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+
+namespace Internship.Connect.QA.API.AutomationTests.RestClientExtension
+{
+    public class ExtendedRestClient : RestClient, IExtendedRestClient
+    {
+        private ILogger<ExtendedRestClient> Logger => LoggerBuilder.GetLogger<ExtendedRestClient>();
+
+        public ExtendedRestClient(Uri uri) : base(uri)
+        {
+        }
+
+        public new async Task<IRestResponse<T>> ExecuteAsync<T>(IRestRequest request, CancellationToken token = default)
+        {
+            LogRequest(request);
+            var response = await base.ExecuteAsync<T>(request, token);
+            LogResponse(response);
+
+            return response;
+        }
+
+        public new async Task<IRestResponse> ExecuteAsync(IRestRequest request, CancellationToken token = default)
+        {
+            LogRequest(request);
+            var response = await base.ExecuteAsync(request, token);
+            LogResponse(response);
+
+            return response;
+        }
+
+        private void LogRequest(IRestRequest request)
+        {
+            Logger.LogInformation($"{request.Method} request to {request.Resource}");
+            if (request.Body != null) Logger.LogInformation($"body: {request.Body}");
+        }
+
+        private void LogResponse(IRestResponse response)
+        {
+            if (response.ErrorException != null)
+            {
+                Logger.LogError($"Error retrieving response. Error message is {response.ErrorException.Message}");
+            }
+            
+            Logger.LogInformation($"Request finished with status code: {response.StatusCode}");
+            if (!string.IsNullOrEmpty(response.Content))
+            {
+                Logger.LogInformation($"{response.Content}");
+            }
+        }
+    }
+}
--- Internship.Connect.QA.API.AutomationTests/Services/BaseService.cs
+++ Internship.Connect.QA.API.AutomationTests/Services/BaseService.cs
@@ -1,4 +1,6 @@
-﻿using Internship.Connect.QA.API.AutomationTests.Constants;
+﻿using System;
+using Internship.Connect.QA.API.AutomationTests.Constants;
+using Internship.Connect.QA.API.AutomationTests.RestClientExtension;
 using Internship.Connect.QA.API.AutomationTests.Utils;
 using RestSharp;
 
@@ -6,13 +8,15 @@ namespace Internship.Connect.QA.API.AutomationTests.Services
 {
     public abstract class BaseService
     {
-        protected readonly IRestClient RestClient;
+        protected readonly IExtendedRestClient RestClient;
+
         private readonly ITaskProcessorAuthService _taskProcessorAuthService;
 
         protected BaseService()
         {
             _taskProcessorAuthService = TaskProcessorAuthService.Instance;
-            RestClient = new RestClient(Endpoints.ConnectUrl);
+            RestClient = new ExtendedRestClient(new Uri(Endpoints.ConnectUrl));
+            // RestClient = new RestClient(Endpoints.ConnectUrl);
         }
 
         protected IRestRequest CreateRestRequest(string uri, Method method, object payload = null)
