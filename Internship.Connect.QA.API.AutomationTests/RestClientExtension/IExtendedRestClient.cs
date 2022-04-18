c82582ecf536be0a578f054b3a28350f939b60fd
AlexanderZnak <67112291+AlexanderZnak@users.noreply.github.com>
2022-04-18 22:25:30 +0300

    Extended rest client (#5)
    * implemented extended restclient
    
    * Added fluentAssertion, refactored services
    
    Co-authored-by: Aliaksandr.Znak <aznak@itventure.com>

---
+++ Internship.Connect.QA.API.AutomationTests/RestClientExtension/IExtendedRestClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Internship.Connect.QA.API.AutomationTests.RestClientExtension
+{
+    public interface IExtendedRestClient : IRestClient
+    {
+        new Task<IRestResponse<T>> ExecuteAsync<T>(IRestRequest request, CancellationToken token = default);
+
+        new Task<IRestResponse> ExecuteAsync(IRestRequest request, CancellationToken token = default);
+    }
+}
